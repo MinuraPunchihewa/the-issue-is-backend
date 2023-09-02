e0fcf608d4cade6d8bc9fcad15989cfc6530b7aa
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-02 12:25:39 +0530

    added the config.py class for dev, test and prod configurations

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+# create the base config
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+# create the development config
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+# create the production config
+class ProductionConfig(Config):
+    pass
+
+
+# create the testing config
+class TestingConfig(Config):
+    pass
