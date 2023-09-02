b181e724da9392dcc0f01198a80849462c59b775
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-02 18:34:10 +0530

    registered JWT when creating the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import DevelopmentConfig
+from flask_jwt_extended import JWTManager
 
 
 def create_app(config=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config)
+
+    # register JWT
+    jwt = JWTManager(app)
 
     # register main blueprint
     from app.blueprints import main as main_bp
