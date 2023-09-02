6c2ec6b81a3c018b9fbfcf42df956db7c7c62e7c
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-02 20:56:56 +0530

    implemented the route for creating a database

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -44,8 +44,41 @@ def login():
 
 
 @main.route('/databases', methods=['PUT'])
+@jwt_required()
 def create_database():
-    pass
+    # get login from jwt
+    login = get_jwt_identity()
+
+    # get server object
+    server = server_objects.get(login)
+
+    # if server object exists, create database
+    if server:
+        # if request is json, get data from json
+        if request.is_json:
+            request_data = request.get_json()
+            database_name = request_data.get('database_name')
+            repository = request_data.get('repository')
+            api_key = request_data.get('api_key')
+
+        # else get data from form
+        else:
+            database_name = request.form.get('database_name')
+            repository = request.form.get('repository')
+            api_key = request.form.get('api_key')
+
+        # if database_name is not empty, create database
+        if database_name and repository and api_key:
+            server.create_database(database_name, 'github', {'repository': repository, 'api_key': api_key})
+            return jsonify({'message': f'Database {database_name} created'}), 200
+
+        # else return error
+        else:
+            return jsonify({'message': 'Either the database name, repository or API key have not been provided'}), 400
+
+    # else return error
+    else:
+        return jsonify({'message': 'Access token is invalid'}), 401
 
 
 @main.route('/databases', methods=['GET'])
