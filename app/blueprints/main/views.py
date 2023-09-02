0daf7a947b2932f78e9017dc8a7bf9baa8739a75
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-02 14:56:39 +0530

    added the skeleton for the essentail routes in main

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -8,6 +8,25 @@ def login():
     pass
 
 
-@main.route('/hello', methods=['GET'])
-def hello():
-    return "Hello!"
+@main.route('/databases', methods=['PUT'])
+def create_database():
+    pass
+
+
+@main.route('/databases', methods=['GET'])
+def get_databases():
+    pass
+
+
+@main.route('/models', methods=['PUT'])
+def create_model():
+    pass
+
+
+@main.route('/issues', methods=['PUT'])
+def create_issue():
+    pass
+
+@main.route('/issues/description', methods=['PUT'])
+def generate_issue_description():
+    pass
