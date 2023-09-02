77b82e97c4a19c85220f115e126ac6c2b2924955
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-02 14:02:29 +0530

    added the create_app() method for running the Flask app

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import DevelopmentConfig
+
+
+def create_app(config=DevelopmentConfig):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # register main blueprint
+    from app.main import main as main_bp
+    app.register_blueprint(main_bp)
+
+    return app
