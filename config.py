4bac00777828eb7ab96500e7bc4f59ffdda0759d
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-04 19:02:02 +0530

    added the PROMPT_TEMPLATE as a configuration

--- config.py
+++ config.py
@@ -8,18 +8,30 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    PROMPT_TEMPLATE = """"""
 
 
 # create the development config
 class DevelopmentConfig(Config):
     DEBUG = True
+    PROMPT_TEMPLATE = """
+    You are a GitHub user and you want to create a new issue. You will be given a title and a description.
+    You are required to elaborate on the issue by providing the following sections: {{sections}}.
+
+    In describing the issue, you should use the lingo: {{lingo}} and the style: {{style}}.
+
+    You should provide clear instructions, carefully craft descriptions, and use structured formatting.
+
+    Title: '{{title}}'
+    Description: '{{description}}'.
+    """
 
 
 # create the production config
 class ProductionConfig(Config):
-    pass
+    PROMPT_TEMPLATE = """"""
 
 
 # create the testing config
 class TestingConfig(Config):
-    pass
+    PROMPT_TEMPLATE = """"""
