54db205cecbc4ccd53cd3903255aad9ec54ea681
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-04 17:07:18 +0530

    renamed servers to server_connections

--- app/blueprints/main/mindsdb_login_manager.py
+++ app/blueprints/main/mindsdb_login_manager.py
@@ -1,3 +1,4 @@
+import logging
 import mindsdb_sdk
 from requests.exceptions import HTTPError
 from flask_jwt_extended import get_jwt_identity
@@ -6,7 +7,7 @@ from flask_jwt_extended import get_jwt_identity
 class MindsDBLoginManager:
     def __init__(self):
         # TODO: save server objects in database/cache
-        self.server_objects = {}
+        self.server_connection_objects = {}
 
     def login(self, login, password):
         try:
@@ -14,22 +15,22 @@ class MindsDBLoginManager:
             server = mindsdb_sdk.connect(login=login, password=password)
 
             # save server object
-            self.save_server_for_login(login=login, server=server)
+            self.save_server_connection_for_login(login=login, server=server)
 
             return True
         except HTTPError as e:
             logging.error(e)
             return False
         
-    def save_server_for_login(self, login, server):
-        self.server_objects[login] = server
+    def save_server_connection_for_login(self, login, server):
+        self.server_connection_objects[login] = server
 
-    def get_server_for_login(self):
+    def get_server_connection_for_login(self):
         # get login from jwt
         login = get_jwt_identity()
 
         # get server object
-        server = self.server_objects.get(login)
+        server = self.server_connection_objects.get(login)
 
         # if server object exists, get databases
         if server:
