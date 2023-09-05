a978086ed4b54d670f06e66c885250f4f9ac95e9
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2023-09-05 08:02:06 +0530

    updated the app to use waitress for serving

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+from waitress import serve
 from app import create_app
 from config import DevelopmentConfig, ProductionConfig, TestingConfig
 
+# pass the config you want to use here, defaults to DevelopmentConfig
 app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, use_reloader=True)
+    serve(app, host='0.0.0.0')
