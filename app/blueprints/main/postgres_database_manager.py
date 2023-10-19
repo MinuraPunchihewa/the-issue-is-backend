edf874680847f15c191344ff8200a5e5bd957854
Minura Punchihewa <minurapunchihewa17@gmail.com>
2023-10-19 23:42:45 +0530

    added method to update user stats

--- app/blueprints/main/postgres_database_manager.py
+++ app/blueprints/main/postgres_database_manager.py
@@ -141,3 +141,14 @@ class PostgresDatabaseManager:
         """
         params = (user_id, repository, owner, issue_url)
         self._execute_query(query, params)
+
+    def update_user_stats(self, user_id: int, is_creation: bool, is_success: bool):
+        column = ('succ' if is_success else 'fail') + ('_cre' if is_creation else '_gen') + '_cnt'
+
+        query = f"""
+            UPDATE users
+            SET {column} = {column} + 1
+            WHERE user_id = %s
+        """
+        params = (user_id,)
+        self._execute_query(query, params)
