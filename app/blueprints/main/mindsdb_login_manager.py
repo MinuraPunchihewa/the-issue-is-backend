859d8b4c2f30c776d5128cbc6215a3b348b6bad7
Minura Punchihewa <minurapunchihewa17@gmail.com>
2024-05-07 15:07:29 +0530

    updated MindsDBConnectionManager to MindsDBConnectionManager

--- app/blueprints/main/mindsdb_login_manager.py
+++ app/blueprints/main/mindsdb_login_manager.py
@@ -1,19 +1,21 @@
 import logging
-import mindsdb_sdk
-from requests.exceptions import HTTPError
+from openai import OpenAI, AuthenticationError
 
 
-class MindsDBLoginManager:
-    def __init__(self):
-        # TODO: save server objects in database/cache
-        self.server_connection_objects = {}
-
-    def login(self, login, password):
+class MindsDBConnectionManager:
+    @staticmethod
+    def connect(base_url, organization, api_key):
         try:
-            # connect to mindsdb server
-            mindsdb_sdk.connect(login=login, password=password)
-            return True
-        except HTTPError as e:
+            # connect to mindsdb inference API
+            mindsdb_inference_client = OpenAI(
+                base_url=base_url,
+                organization=organization,
+                api_key=api_key
+            )
+
+            mindsdb_inference_client.models.list()
+            return mindsdb_inference_client
+        except AuthenticationError as e:
             logging.error(e)
-            return False
+            return None
         
